73ca390fccc3b0264018ce0141410621a9a5bedf
kristobalus <ildar.tamendarov@gmail.com>
2023-09-30 11:33:49 +0600

    chore: docker image build script

---
+++ build-image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Read the version from package.json
+VERSION=1.0.0
+IMAGE=kristobalus/nitter:$VERSION
+echo "building image $IMAGE using buildx for multi-arch..."
+
+docker buildx create --use --name buildx_instance --driver docker-container --bootstrap
+docker buildx build -f ./Dockerfile \
+		--progress=plain \
+		--build-arg VERSION="$VERSION" \
+		--label "build-tag=build-artifact" \
+		--platform linux/amd64 \
+		-t $IMAGE \
+		--push . || { echo "failed to build docker image"; exit 1; }
+
+# commented out to keep cached layers
+# docker buildx rm buildx_instance
+docker image prune -f --filter label=build-tag=build-artifact
