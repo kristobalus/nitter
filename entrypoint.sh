593f9037debf80972e72de9382d1e978ca6d750c
kristobalus <ildar.tamendarov@gmail.com>
2023-12-01 10:18:25 +0600

    feat: fly.io vm

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
 
 echo "$NITTER_GUEST_ACCOUNTS_URL"
-cp /config/nitter.conf /src/nitter.conf
+# cp /config/nitter.conf /src/nitter.conf
 cat /src/nitter.conf
 wget -O /src/guest_accounts.json "$NITTER_GUEST_ACCOUNTS_URL"
-./nitter
+
+# Redis host and port
+MAX_ATTEMPTS=30  # 5 minutes with 10-second intervals
+REDIS_HOST="127.0.0.1"
+REDIS_PORT=6379
+
+attempt=1
+while [ $attempt -le $MAX_ATTEMPTS ]
+do
+    echo "Checking if Redis is up (Attempt: $attempt)..."
+    # Try to ping the Redis server
+    if redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" ping | grep -q "PONG"
+    then
+        echo "Redis is up! Starting nitter"
+        ./nitter
+    fi
+
+    # Wait for 10 seconds before retrying
+    sleep 10
+    attempt=$((attempt+1))
+done
+
+echo "Redis did not start within the expected time."
+exit 1
